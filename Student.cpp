ebdadd2027b80b2cf46e9254b829e1f6b3e610ad
Kamel R <kamel.r@wayne.edu>
2025-09-08 17:59:07 -0400

    split the .h code from cpp

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,40 @@
 #include "Student.h"
+
+string Student::getStudentID()
+{
+	return studentID;
+}
+
+void Student::setStudentID(string sid)
+{   // TODO: add validation later 
+	studentID = sid;
+
+}
+
+string Student::getFirstName()
+{
+	return firstName;
+}
+
+void Student::setFirstName(string fn)
+{
+	firstName = fn;
+}
+
+string Student::getLastName()
+{
+	return lastName;
+}
+
+void Student::setLastName(string ln)
+{
+	lastName = ln;
+}
+
+void Student::printStudentInfo()
+{
+	cout << getFirstName() << endl;
+	cout << getLastName() << endl;
+	cout << getStudentID() << endl;
+
+}
--- Student.h
+++ Student.h
@@ -14,41 +14,19 @@ private:
 public: 
 	// Set and get functions 
 	// student ID
-	string getStudentID()
-	{
-		return studentID;
-	}
-
-	void setStudentID(string sid)
-	{   // TODO: add validation later 
-		studentID = sid; 
+	string getStudentID();
+	void setStudentID(string sid);
 	
-	}
 	// first name
-	string getFirstName()
-	{
-		return firstName;
-	}
-	void setFirstName(string fn)
-	{
-		firstName = fn;
-	}
+	string getFirstName();
+	
+	void setFirstName(string fn);
+	
     // last name
-	string getLastName()
-	{
-		return lastName;
-	}
-	void setLastName(string ln)
-	{
-		lastName = ln;
-	}
-
-	void printStudentInfo()
-	{
-		cout << getFirstName()<<endl;
-		cout << getLastName() << endl;
-		cout << getStudentID() << endl;
-
-	}
+	string getLastName();
+	
+	void setLastName(string ln);
+	void printStudentInfo();
+	
 };
 
